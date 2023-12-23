fbb07cdf3f132f0d6d3ac2194fdd7711736e5e97
Daniel Lee <danlee1996@proton.me>
2023-12-23 13:59:40 -0700

    adding comments to migration

--- db/migrate/20231223034301_create_join_table_humen_cats.rb
+++ db/migrate/20231223034301_create_join_table_humen_cats.rb
@@ -1,6 +1,8 @@
 class CreateJoinTableHumenCats < ActiveRecord::Migration[7.1]
   def change
     create_join_table :humen, :cats do |t|
+      # From the docs: https://api.rubyonrails.org/v7.1.2/classes/ActiveRecord/Associations/ClassMethods.html#method-i-has_and_belongs_to_many
+      # It’s also a good idea to add indexes to each of those columns to speed up the joins process. However, in MySQL it is advised to add a compound index for both of the columns as MySQL only uses one index per table during the lookup.
       t.index [:human_id, :cat_id]
       t.index [:cat_id, :human_id]
     end
